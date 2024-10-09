1d14743f907cd9b131765fa003bf1f06dbc950d0
ChristopherP-C <christopherp.c.2day@gmail.com>
2024-10-09 04:01:59 -0500

    added questions up to additional content and inquirer setup

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        //README title prompt
+        type: `input`,
+        name: `title`,
+        message: "Input the title of your README, typically your project title",
+    },
+    {
+        //README motive
+        type: `input`,
+        name: `motive`,
+        message: "In 1 to 2 sentences, describe your motive for making this project",
+    },
+    {
+        //README why did you make this project
+        type: `input`,
+        name: `whyMake`,
+        message: "In 1 to 2 sentences, describe why you made this project",
+    },
+    {
+        //README what this solved
+        type: `input`,
+        name: `solved`,
+        message: "In 1 to 2 sentences, describe what this project accomlpished solving",
+    },
+    {
+        //README what was learned
+        type: `input`,
+        name: `learned`,
+        message: "In 1 to 2 sentences, describe what you learned while making this project",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
